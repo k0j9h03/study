585d6ffb29f52f0fe27d32c4159fbcfde255a9ea
k0j9h03 <k0j9h03@naver.com>
2025-05-27 14:00:05 +0900

    third work

--- 03/sketch.js
+++ 03/sketch.js
@@ -1,29 +1,50 @@
-let s;
 let outer = [];
-let outerNumber = 19;
-let radius = 80;
+let inner = [];
+let outerNumber = 10;
+let innerNumber = 3;
+let radius = 100;
+let radius2 = 30;
+let speed = 0.31;
 
 function setup(){
-    // createCanvas(innerWidth,innerHeight);
-    createCanvas(700,700)
+    createCanvas(700, 700);
     angleMode(DEGREES);
 
-}
+    let unitAngle1 = 360 / outerNumber;
+    let unitAngle2 = 360 / innerNumber;
 
+    for(let i = 0; i < outerNumber; i++) {
+        let angle = unitAngle1 * i;
+        outer[i] = new Star(angle, radius);
+    }
+
+    for(let i = 0; i < innerNumber; i++) {
+        let angle = unitAngle2 * i;
+        inner[i] = new Star(angle, radius2);
+    }
+}
 
 function draw(){
     background(220);
-    translate(width/2,height/2);
-    let TWO_PI = 360;
+    translate(width / 2, height / 2);
 
-    for(let i=0;  i<outerNumber; i++ ){
-        let unitAngle = TWO_PI/outerNumber;
-        let drawenAngle = unitAngle * i;
-        outer[i] = new Star(drawenAngle , radius);
+    // 외곽 고정
+    for(let i = 0; i < outerNumber; i++) {
         outer[i].update();
         outer[i].display();
     }
 
-    
+    // 내부 회전
+    for(let i = 0; i < innerNumber; i++) {
+        inner[i].angle += 1;
+        inner[i].update();
+        inner[i].display();
+    }
 
+    // 연결선
+    for(let i = 0; i < outerNumber; i++) {
+        for(let j = 0; j < innerNumber; j++) {
+            line(outer[i].x, outer[i].y, inner[j].x, inner[j].y);
+        }
+    }
 }
--- 03/star.js
+++ 03/star.js
@@ -1,18 +1,17 @@
-class Star{
-    constructor(angle, r){
 
-        this.r = r;
+class Star {
+    constructor(angle, r){
         this.angle = angle;
+        this.r = r;
     }
 
     update(){
-        this.x = this.r*cos(this.angle);
-        this.y = this.r*sin(this.angle);
+        this.x = this.r * cos(this.angle);
+        this.y = this.r * sin(this.angle);
     }
 
     display(){
-        // line (0,0, this.x, this.y);
-        ellipse(this.x, this.y, 10, 10);
-
+        // line(0, 0, this.x, this.y);
+        // ellipse(this.x, this.y, 10, 10);
     }
 }
